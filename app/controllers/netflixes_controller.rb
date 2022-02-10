bca91f91124a635e86ea534ca86295cf3a45a4c4
Luis Roberto Machado(Beto) <60554611+beto-machado@users.noreply.github.com>
2022-02-09 21:56:29 -0300

    Update netflixes_controller.rb

--- app/controllers/netflixes_controller.rb
+++ app/controllers/netflixes_controller.rb
@@ -1,21 +1,25 @@
 # frozen_string_literal: true
+module Api
+  
+  module V1
 
-class NetflixesController < ApplicationController
+    class NetflixesController < ApplicationController
 
-  def listing
-    if search.present?
-      @netflixes = Netflix.search(search).sorted.page(params[:page])
-    else
-      @netflixes = Netflix.sorted.page(params[:page])
+      def listing
+        if search.present?
+          @netflixes = Netflix.search(search).sorted.page(params[:page])
+        else
+          @netflixes = Netflix.sorted.page(params[:page])
+        end
+
+        paginate json: @netflixes, only: [:id, :title, :type, :release_year, :country, :date_added, :description]
+      end
+
+      private
+
+      def search
+        params[:search]
+      end
     end
-
-    paginate json: @netflixes, only: [:id, :title, :type, :release_year, :country, :date_added, :description]
   end
-
-  private
-
-  def search
-    params[:search]
-  end
-
 end
